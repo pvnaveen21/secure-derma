30805dd5b7810be85dcb8cf421bf42e067ec1694
naveen <pvnaveen21@gamil.com>
2026-03-23 15:52:35 +0530

    vercel bug commit

--- back-end/config/settings.py
+++ back-end/config/settings.py
@@ -56,6 +56,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:4201",
     "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://secure-derma-admin.vercel.app",
 ]
 extra_cors_origins = [
     origin.strip()
