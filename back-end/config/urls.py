e407ca5f8fc7be4488e27d53108bc96fe420e291
naveen <pvnaveen21@gamil.com>
2026-03-24 10:52:59 +0530

    Fix Render backend deployment

--- back-end/config/urls.py
+++ back-end/config/urls.py
@@ -14,17 +14,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path ,include
+from django.http import JsonResponse
+from django.urls import include, path
 
 from api import admin_router, user_router
-from django.conf.urls.static import static
-from django.conf import settings
+
+
+def healthcheck(_request):
+    return JsonResponse({"status": "ok"})
 
 
 urlpatterns = [
+    path('', healthcheck),
     path('dbadmin/', admin.site.urls),
     path('api/admin/', include(admin_router)),
     path('api/', include(user_router)),
-    
-]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
